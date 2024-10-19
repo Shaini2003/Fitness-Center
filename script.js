a7e4742a8455dd40c302291fe2ef1dd4a3deb98f
Shaini2003 <shinitharushika2020@gmail.com>
2024-10-19 15:45:54 +0530

    15th commit

--- script.js
+++ script.js
@@ -21,3 +21,27 @@ btns.forEach((btn,i) =>{
         sliderNav(i);
     })
 })
+
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView:3,
+    spaceBetween:30,
+    grabCursor:true,
+    autoplay:{
+        delay:2500,
+        disableOnInteraction:false,
+        pauseOnmouseEnter:true,
+    },
+    slidesPerGroup:1,
+    loop:true,
+    loopFillGroupWithBlank:true,
+    pagination:{
+        el:".swiper-paganation",
+        clickable:true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
