a7c95b537591d96983b7897905349f1113bd9f6f
Shaini2003 <shinitharushika2020@gmail.com>
2024-10-23 13:29:50 +0530

    36th commit

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,73 +8,79 @@
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" />
 </head>
+
 <body>
-<div class="slide-menu">
-    <div class="brand-name">
-        <h1>FitZone</h1>
+    <div class="slide-menu">
+        <div class="brand-name">
+            <h1>FitZone</h1>
+        </div>
+        <ul>
+            <li><a href="#">Dashboard</a></li>
+            <li><a href="users.php">Users</a></li>
+            <li><a href="staff.php">Staff</a></li>
+            <li><a href="payment1.php">Class-Payments</a></li>
+            <li><a href="payemt2.php">Membership-payments</a></li>
+            <li><a href="FAQ.php">FAQs</a></li>
+            <li><a href="classes.php">Classes</a></li>
+            <li><a href="reviews.php">Customer Reviews</a></li>
+        </ul>
     </div>
-    <ul>
-        <li>Dashboard</li>
-        <li>Users</li>
-        <li>Staff</li>
-        <li>Class-Payments</li>
-        <li>Membership-payments</li>
-        <li>FAQs</li>
-        <li>Classes</li>
-        <li>Customer Reviews</li>
-    </ul>
-</div>
-<div class="container">
-    <div class="header">
-        <div class="nav">
-            <center>
-            <div class="user">
-               <img src="user-1.png" class="image-case">
-               <h1>Admin Dashboard</h1>
-            </div></center>
+    <div class="container">
+        <div class="header">
+            <div class="nav">
+                <center>
+                    <div class="user">
+                        <img src="user-1.png" class="image-case">
+                        <h1>Admin Dashboard</h1>
+                    </div>
+                </center>
+            </div>
         </div>
+        <div class="content">
+            <div class="cards">
+                <div class="card">
+                    <div class="box">
+                        <h1>2194</h1>
+                        <h3>Users</h3>
+                    </div>
+                    <div class="icon-case">
+                        <img src="group.png" style="width: 100px;height: 100px;">
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <h1>80</h1>
+                        <h3>Classes</h3>
+                    </div>
+                    <div class="icon-case">
+                        <img src="training.png" style="width: 100px;height: 100px;">
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <h1>546</h1>
+                        <h3>Staff</h3>
+                    </div>
+                    <div class="icon-case">
+                        <img src="teamwork.png" style="width: 100px;height: 100px;">
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <h1>200+</h1>
+                        <h3>Reviews</h3>
+                    </div>
+                    <div class="icon-case">
+                        <img src="reviews.png" style="width: 100px;height: 100px;">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
     </div>
-<div class="content">
-    <div class="cards">
-        <div class="card">
-            <div class="box">
-                <h1>2194</h1>
-                <h3>Users</h3>
-            </div>
-            <div class="icon-case">
-                <img src="group.png" style="width: 100px;height: 100px;">
-            </div>
-        </div>
-        <div class="card">
-            <div class="box">
-                <h1>80</h1>
-                <h3>Classes</h3>
-            </div>
-            <div class="icon-case">
-                <img src="training.png" style="width: 100px;height: 100px;">
-            </div>
-        </div>
-        <div class="card">
-            <div class="box">
-                <h1>546</h1>
-                <h3>Staff</h3>
-            </div>
-            <div class="icon-case">
-                <img src="teamwork.png"style="width: 100px;height: 100px;">
-            </div>
-        </div>
-        <div class="card">
-            <div class="box">
-                <h1>200+</h1>
-                <h3>Reviews</h3>
-            </div>
-            <div class="icon-case">
-                <img src="reviews.png"style="width: 100px;height: 100px;">
-            </div>
-        </div>
-    </div>
-</div>
-</div>
+
 
 </body>
+
 </html>
---
+++ admin/users.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Users</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container my-5">
+        <h2>List of Users</h2>
+        <a class="btn btn-primary" href="/admin/create.php" role="button">New User</a><br>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>UserId</th>
+                    <th>Name</th>
+                    <th>Phone</th>
+                    <th>Email</th>
+                    <th>Password</th>
+                    <th>UserType</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $serverName = "localhost";
+                $dbUsername = "Shaini_tharushika";
+                $dbPassword = "shaini12@MT";
+                $dbName = "fitness_center";
+
+                $conn = mysqli_connect($serverName, $dbUsername,$dbPassword,$dbName);
+                if(!$conn){
+                    die("Connection failed : ".mysqli_connect_error());
+                
+                }
+
+                $sql= "SELECT * FROM users";
+                $result = $conn->query($sql);
+
+                if(!$result){
+                    die("Invalid query: ".$conn->error);
+                }
+                while($row =$result->fetch_assoc()){
+                    echo "
+                    <tr>
+                    <td>$row[usersId]</td>
+                    <td>$row[usersName]</td>
+                    <td>$row[usersPhone]</td>
+                    <td>$row[usersEmail]</td>
+                    <td>$row[usersPwd]</td>
+                    <td>$row[userType]</td>
+                    <td>
+                        <a class='btn btn-primary btn-sm'href='/admin/edit.php?usersId=$row[usersId]'>Edit</a>
+                        <a class='btn btn-danger btn-sm'href='/admin/delete.php?usersId=$row[usersId]'>Delete</a>
+                    </td>
+                </tr> ";
+                }
+                ?>
+                
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
