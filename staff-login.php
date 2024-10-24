a691eca2b858fd8e98d0a30adea0930ea0679845
Shaini2003 <shinitharushika2020@gmail.com>
2024-10-24 19:26:17 +0530

    46th commit

---
+++ staff-login.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <header> <!---Create a Logo-->
+        <a href="index.php" class="logo">Fitzone Staff Dashboard</a>
+        <!---Create a navigation bar-->
+        <ul class="navbar">
+            <li><a href="#users">User Details</a></li>
+            <li><a href="#classes">SERVICES</a></li>
+            <li><a href="#FAQ">FAQs</a></li>
+            <li><a href="#review">User Reviews</a></li>
+            
+        </ul>
+    </header> 
+    <section id="users">
+    <div class="container my-5">
+        <h2>List of Users</h2>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>UserId</th>
+                    <th>Name</th>
+                    <th>Phone</th>
+                    <th>Email</th>
+                    <th>Password</th>
+                    <th>UserType</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $serverName = "localhost";
+                $dbUsername = "Shaini_tharushika";
+                $dbPassword = "shaini12@MT";
+                $dbName = "fitness_center";
+
+                $conn = mysqli_connect($serverName, $dbUsername,$dbPassword,$dbName);
+                if(!$conn){
+                    die("Connection failed : ".mysqli_connect_error());
+                
+                }
+
+                $sql= "SELECT * FROM users";
+                $result = $conn->query($sql);
+
+                if(!$result){
+                    die("Invalid query: ".$conn->error);
+                }
+                while($row =$result->fetch_assoc()){
+                    echo "
+                    <tr>
+                    <td>$row[usersId]</td>
+                    <td>$row[usersName]</td>
+                    <td>$row[usersPhone]</td>
+                    <td>$row[usersEmail]</td>
+                    <td>$row[usersPwd]</td>
+                    <td>$row[userType]</td>
+                
+                </tr> ";
+                }
+                ?>
+                
+            </tbody>
+        </table>
+    </div>
+    </section>
+
+    </section>
+    <section id="classes">
+    <div class="container my-5">
+    <h2 style="text-align: center;font-size: 40px;"> Services</h2>
+    
+    <table class="table">
+        <thead style="font-size: 20px;">
+            <tr>
+                <th>Class ID</th>
+                <th>Class Name</th>
+                <th>Class Price</th>
+                <th>Class Image</th>
+            </tr>
+        </thead>
+        <tbody style="font-size: 18px;">
+            <?php
+            $serverName = "localhost";
+            $dbUsername = "Shaini_tharushika";
+            $dbPassword = "shaini12@MT";
+            $dbName = "fitness_center";
+
+            $conn = mysqli_connect($serverName, $dbUsername,$dbPassword,$dbName);
+            if(!$conn){
+                die("Connection failed : ".mysqli_connect_error());
+            
+            }
+            $sql= "SELECT * FROM classes";
+            $result = $conn->query($sql);
+
+            if(!$result){
+                die("Invalid query: ".$conn->error);
+            }
+            while($row =$result->fetch_assoc()){
+                echo "
+                <tr>
+                    <td>{$row['id']}</td>
+                    <td>{$row['name']}</td>
+                    <td>{$row['price']}</td>
+                    <td><img src='data:image/jpeg;base64," . base64_encode($row['image']) . "' alt='Class Image' style='width:100px; height:auto;'/></td>
+                </tr>";
+            }
+
+            ?>
+        </tbody>
+    </table>
+</div>
+    </section>
+
+    <section id="FAQ">
+    <div class="container my-5">
+        <h2 style="text-align: center;">List of FAQs</h2>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Email</th>
+                    <th>FAQ</th>
+                    
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $serverName = "localhost";
+                $dbUsername = "Shaini_tharushika";
+                $dbPassword = "shaini12@MT";
+                $dbName = "fitness_center";
+
+                $conn = mysqli_connect($serverName, $dbUsername,$dbPassword,$dbName);
+                if(!$conn){
+                    die("Connection failed : ".mysqli_connect_error());
+                
+                }
+
+                $sql= "SELECT * FROM user_emails";
+                $result = $conn->query($sql);
+
+                if(!$result){
+                    die("Invalid query: ".$conn->error);
+                }
+                while($row =$result->fetch_assoc()){
+                    echo "
+                    <tr>
+                    <td>$row[Id]</td>
+                    <td>$row[email]</td>
+                    <td>$row[FAQ]</td>
+                    
+                    
+                </tr> ";
+                }
+                ?>
+                
+            </tbody>
+        </table>
+    </div>
+    </section>
+
+    <section id="review">
+    <div class="container my-5">
+        <h2>List of Reviews</h2>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Review_Id</th>
+                    <th>User_Name</th>
+                    <th>User_Rating</th>
+                    <th>User_Review</th>
+                    <th>Date Time</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $serverName = "localhost";
+                $dbUsername = "Shaini_tharushika";
+                $dbPassword = "shaini12@MT";
+                $dbName = "fitness_center";
+
+                $conn = mysqli_connect($serverName, $dbUsername,$dbPassword,$dbName);
+                if(!$conn){
+                    die("Connection failed : ".mysqli_connect_error());
+                
+                }
+
+                $sql= "SELECT * FROM review_table";
+                $result = $conn->query($sql);
+
+                if(!$result){
+                    die("Invalid query: ".$conn->error);
+                }
+                while($row =$result->fetch_assoc()){
+                    echo "
+                    <tr>
+                    <td>$row[review_id]</td>
+                    <td>$row[user_name]</td>
+                    <td>$row[user_rating]</td>
+                    <td>$row[user_review]</td>
+                    <td>$row[datetime]</td>
+                    <td>
+                        
+                        <a class='btn btn-danger btn-sm' href='/admin/review-delete.php?review_id=$row[review_id]'>Delete</a>
+                    </td>
+                </tr> ";
+                }
+                ?>
+                
+            </tbody>
+        </table>
+    </div>
+    </section>
+</body>
+</html>
+   
--- staff.php
+++ staff.php
@@ -7,26 +7,26 @@
     <link rel="stylesheet" href="staff.css">
 
 </head>
-<body style="background-image: url(Media/background.jpeg);background-size:cover;background-position: center center;">
+<body style="background-image: url(Media/background\ image.jpg);background-size:cover;background-position: center center;">
     <a href="index.php" class="logo">Fitzone <span>Fitness </span>Center</a>
 
     <p style="color:#2980b9;font-weight: 900;font-size: 40px;">Elevate your fitness</p><br><br><br><br>
     <div class="container">
         <div class="login-form">
             
-            <h1 style="text-align: center;font-weight:900;">Login</h1>
+            <h1 style="text-align: center;font-weight:900;color: white;">Staff Login</h1>
         
             <form action ="includes/login.inc.php" method="post" ><div style="text-align: center;">
-                <label for="email" style="font-size: 18px;text-align:left;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Email Address:</label>
+                <label for="email" style="font-size: 18px;text-align:left;color: white;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Email Address:</label>
                 <input type="email" id="email" name="email" placeholder="Email"><br><br>
-                <label for="password" style="font-size: 18px;text-align:left;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Password:</label>
+                <label for="password" style="font-size: 18px;text-align:left;color: white;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Password:</label>
                 <input type="password" id="password" name="password" placeholder="Password">
-                <p style="font-weight: bolder;font-size: 18px;">Forget Password?</p><center>
+                <p style="font-weight: bolder;font-size: 18px;color: white;">Forget Password?</p><center>
                 
-                    <a href="" class="button">LOGIN</a></center></div>
+                    <a href="staff-login.php" class="button">LOGIN</a></center></div>
             </form>
         
-            <p style="font-weight: bolder;">Don't have an account? &nbsp;<a href="createAccount.php" style="color:black;font-weight: bold;">Sign Up</a></p>
+            <p style="font-weight: bolder;color: white;">Don't have an account? &nbsp;<a href="createAccount.php" style="color:black;font-weight: bold;color: white;">Sign Up</a></p>
         </div>
     </div>
 
