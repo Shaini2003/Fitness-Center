1403e0f53273753b316289a542d8199dc7a95baa
Shaini2003 <shinitharushika2020@gmail.com>
2024-10-25 16:21:17 +0530

    49 th commit

--- google-login/google.php
+++ google-login/google.php
@@ -4,38 +4,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google-signin-client_id" content="348151458747-2lll62ao2im3chloi122v9pn187dgl2e.apps.googleusercontent.com">
     <title>Login</title>
-    <link rel="stylesheet" href="staff.css">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" rel="google.css">
 </head>
 
-<body style="background-image: url(Media/background\ image.jpg);background-size:cover;background-position: center center;">
-    <a href="index.php" class="logo">Fitzone <span>Fitness </span>Center</a>
+<body><center>
+  <h2 class="alert alert-primary">Sign in with Google</h2>
+  <div class="g-signin2" data-onsuccess="onSignIn"></div>
+  <div class="data">
+    <p>Name</p>
+    <p id="name" class="alert alert-success"></p>
+    <p>Profile Pic</p>
+    <img id="image" class="rounded-circle" width="100" height="100" />
+    <p>Email</p>
+    <p id="email"></p>
+    <button type="button" class="btn btn-danger" onclick="signOut();">Sign Out</button>
+  </div>
 
-    <p style="color:#2980b9;font-weight: 900;font-size: 40px;">Elevate your fitness</p><br><br><br><br>
-    <div class="container">
-        <div class="login-form">
-
-            <h1 style="text-align: center;font-weight:900;color: white;"><img src="Media/search.png" width="40px">&nbsp;SignIn with Google</h1>
-
-            <form action="includes/login.inc.php" method="post">
-                <div style="text-align: center;">
-                    <label for="email" style="font-size: 18px;text-align:left;color: white;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Email Address:</label>
-                    <input type="email" id="email" name="email" placeholder="Email"><br><br>
-                    <label for="password" style="font-size: 18px;text-align:left;color: white;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Password:</label>
-                    <input type="password" id="password" name="password" placeholder="Password">
-                    <center>
-                        <a href="staff-login.php" class="button" style="background-color: black;"><img src="Media/search.png" width="20px">&nbsp;SIGN IN</a>
-                    </center>
-                </div>
-                </center>
-            </form>
-
-            <p style="font-weight: bolder;color: white;">Don't have an account? &nbsp;<a href="createAccount.php" style="color:black;font-weight: bold;color: white;">Sign Up</a></p>
-        </div>
-    </div>
-
-
+    <script src="google.js"></script>
+<script src="https://apis.google.com/js/platform.js" async defer></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+</center>
 </body>
 
 </html>
